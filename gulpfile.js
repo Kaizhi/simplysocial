c8d5136e54b042475206d979fa7b1f2d66d257df
Kaizhi Wei <kaizhi@yahoo-inc.com>
2015-08-19 22:32:20 -0700

    initial commit - gulp watch/sass/sprites, simple express server serving hbs templates

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sprity = require('sprity');
+
+var sourcePaths = {
+  styles: ['scss/**/*.scss']
+};
+
+var distPaths = {
+  styles: 'public/css'
+};
+
+
+// generate sprite.png and _sprite.scss
+gulp.task('sprites', function () {
+  return sprity.src({
+    src: 'icons/**/*.{png,jpg}',
+    style: '_sprites.scss',
+    cssPath: '../img/',
+    processor: 'sass',
+  })
+  .pipe(gulpif('*.png', gulp.dest('public/img/'), gulp.dest('scss/')));
+});
+
+// Compile scss files to css
+gulp.task('sass', function () {
+  gulp.src( sourcePaths.styles )
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: true
+    }))
+    .pipe(gulp.dest( distPaths.styles ));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(sourcePaths.styles, ['sass']);
+});
+
+gulp.task('build', ['sprites', 'sass']);
+
+gulp.task('default', ['build', 'watch']);
